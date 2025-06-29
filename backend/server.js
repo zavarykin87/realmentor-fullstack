bedcd47d02183623dba4cf02c44c636f964437ec
zavarykin <a.zavarykin@ase-ec.ru>
2025-06-29 09:52:19 +0300

    изменил структуру проекта

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello Andrew!')
 })
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`)
 })
